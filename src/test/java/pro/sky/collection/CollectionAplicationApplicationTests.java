1bb8c9c04805aa3511777ba5ad4925b5d3f4b382
skyvlados <vladislav.repin@skyeng.ru>
2021-12-21 22:56:23 +0300

    Initial commit

---
+++ src/test/java/pro/sky/collection/CollectionAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
